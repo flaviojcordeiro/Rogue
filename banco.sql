c0cdc72d7166d80bdbd7180bd2c4ee9d7d40b658
flaviojcordeiro <149179836+flaviojcordeiro@users.noreply.github.com>
2024-04-15 10:50:55 -0300

    Script Banco de Dados

--- banco.sql
+++ banco.sql
@@ -1,7 +1,34 @@
-CREATE TABLE users (
+CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    nome VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    birthdate DATE NOT NULL
+    senha VARCHAR(255) NOT NULL,
+    data_nascimento DATE NOT NULL,
+    is_admin BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE categorias_roupas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE roupas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    categoria_id INT,
+    genero ENUM('masculino', 'feminino', 'unissex') NOT NULL,
+    foto VARCHAR(255), 
+    preco DECIMAL(10, 2) NOT NULL,
+    quantidade_estoque INT NOT NULL,
+    FOREIGN KEY (categoria_id) REFERENCES categorias_roupas(id)
+);
+
+CREATE TABLE roupas_likes (
+    usuario_id INT,
+    roupa_id INT,
+    gostou BOOLEAN,
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+    FOREIGN KEY (roupa_id) REFERENCES roupas(id),
+    PRIMARY KEY (usuario_id, roupa_id)
 );
