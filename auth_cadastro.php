4b71e9b0515c5d2026636f39583246c8c6e0c708
Erick Menezes <erickdemenezes04@gmail.com>
2024-04-22 18:30:51 -0300

    regex

--- auth_cadastro.php
+++ auth_cadastro.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             exit();
         }
 
-        if (!preg_match("/^[0-9]{3}\.[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}$/", $cpf)) {
+        if (!preg_match("/^[0-9]{3}\.?[0-9]{3}\.?[0-9]{3}\-?[0-9]{2}$/", $cpf)) {
             echo "CPF inválido.";
             exit();
         }
--- carrinho.php
+++ carrinho.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 if (!isset($_SESSION['nome'])) {
     header("Location: login.php");
